8f7ac0b7e94241c14bb8a0a8654114bd100ac812
Mikołaj Mejer <73598279+randomLando@users.noreply.github.com>
2023-03-23 14:55:37 +0100

    Add files via upload

---
+++ Neon cyfrowy/Neon cyfrowy/Neon cyfrowy.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream input("instrukcje.txt");
+    string command;
+    char letter;
+    string result = "";
+
+    while (input >> command >> letter) {
+        if (command == "DOPISZ") {
+            result += letter;
+        }
+        else if (command == "ZMIEN") {
+            result[result.size() - 1] = letter;
+        }
+        else if (command == "USUN") {
+            result.pop_back();
+        }
+        else if (command == "PRZESUN") {
+            for (int i = 0; i < result.size(); i++) {
+                if (result[i] == letter) {
+                    if (letter == 'Z') {
+                        result[i] = 'A';
+                    }
+                    else {
+                        result[i]++;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
